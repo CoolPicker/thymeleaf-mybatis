da49c86e927eabe9779f004f0f1b4ebb9c57effc
niuya <niuya312@163.com>
2018-05-22 17:59:45 +0800

    新增单元测试样例

--- src/test/java/com/nya/ThymeleafMybatisApplicationTests.java
+++ src/test/java/com/nya/ThymeleafMybatisApplicationTests.java
@@ -1,7 +1,11 @@
 package com.nya;
 
+import com.nya.controller.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ThymeleafMybatisApplicationTests {
 
+	private static Logger logger = LoggerFactory.getLogger(ThymeleafMybatisApplication.class);
+
+	@Autowired
+	private UserController userController;
+
 	@Test
 	public void contextLoads() {
+		String hello = userController.sayHello();
+		logger.info("SpringBootTest:"+hello);
 	}
 
 }
